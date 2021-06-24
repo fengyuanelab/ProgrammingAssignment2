70071f9e248908eb9df392ac08f114f953340615
fengyuanelab <69617832+fengyuanelab@users.noreply.github.com>
2021-06-24 13:14:22 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inv <- NULL  ##prevent old inv being used
+  ## set x to new value in new parent environment and reset inv to NULL in parent environment
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  get <- function (){x}
-  setInverse <- function(inverse) {inv <<- inverse}
-  getInverse <- function() {inv}
+  get <- function (){x} #get function that retrieves x
+  setInverse <- function(inverse) {inv <<- inverse} # set inv to new value 
+  getInverse <- function() {inv} #retrieves inv
+  ## return the above function to be used by cachesolve
   list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
@@ -19,10 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getInverse()
+  inv <- x$getInverse() # retrieving stored inverse
+  # check if iverse has been calculated, returns if there 
   if(!is.null(inv)){
     message("getting cached data")
   }
+  
+  #calculating, storing and returning inv if it's not already calculated
    mat <- x$get() 
    inv <- solve(mat, ...)
    x$setInverse (inv)
